cdba528c1f78ff6a081c176edb284e7d219a36bf
kevinSn-5000 <179320367+kevinSn-5000@users.noreply.github.com>
2024-11-04 18:37:31 -0600

    Create files, basic functions and display

---
+++ javaScript.js
@@ -0,0 +1,21 @@
+function add(a , b){
+    return a + b;
+}
+function subtract(a , b){
+    return a - b;
+}
+function multiply(a , b){
+    return a * b;
+}
+function divide(a , b){
+    return a / b;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber){
+    firstNumber + operator + secondNumber
+    
+}
