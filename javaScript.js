830bbe50ad96aafeced34fab7889f98381fdb9ee
kevinSn-5000 <179320367+kevinSn-5000@users.noreply.github.com>
2024-12-04 16:42:23 -0600

    Complete the basic logic for the calculator
    Now the calculator can perform the basic math operations and display
    the result.

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,13 @@
-function add(a , b){
+function addNumbers(a , b){
     return a + b;
 }
-function subtract(a , b){
+function subtractNumbers(a , b){
     return a - b;
 }
-function multiply(a , b){
+function multiplyNumbers(a , b){
     return a * b;
 }
-function divide(a , b){
+function divideNumbers(a , b){
     return a / b;
 }
 
@@ -15,7 +15,74 @@ let firstNumber;
 let operator;
 let secondNumber;
 
-function operate(firstNumber, operator, secondNumber){
-    firstNumber + operator + secondNumber
-    
+
+
+const display = document.querySelector(".display");
+function changeDisplay (symbol){
+    display.textContent += symbol;
+    console.log(display.textContent);
+
 }
+
+
+let added = document.querySelector(".add")
+added.addEventListener("click", () => {
+    firstNumber = +display.textContent;
+    operator = "+"
+    display.textContent = "";
+})
+
+let subtract = document.querySelector(".subtract")
+subtract.addEventListener("click", () => {
+    firstNumber = +display.textContent;
+    operator = "-"
+    display.textContent = "";
+})
+
+let multiply = document.querySelector(".multiply")
+multiply.addEventListener("click", () => {
+    firstNumber = +display.textContent;
+    operator = "*"
+    display.textContent = "";
+})
+
+let divide = document.querySelector(".divide")
+divide.addEventListener("click", () => {
+    firstNumber = +display.textContent;
+    operator = "/"
+    display.textContent = "";
+})
+
+let result;
+let equalButton = document.querySelector(".equalButton")
+
+equalButton.addEventListener("click", () => {
+
+    secondNumber = +display.textContent;
+    
+    switch(operator){
+        case "+":
+        result = addNumbers(firstNumber, secondNumber)
+        firstNumber = result;
+        display.textContent = result;
+        break;
+
+        case "-":
+        result = subtractNumbers(firstNumber, secondNumber)
+        firstNumber = result;
+        display.textContent = result;
+        break;
+
+        case "*":
+        result = multiplyNumbers(firstNumber, secondNumber)
+        firstNumber = result;
+        display.textContent = result;
+        break;
+        
+        case "/":
+        result = divideNumbers(firstNumber, secondNumber)
+        firstNumber = result;
+        display.textContent = result;
+        break;
+    }
+})
